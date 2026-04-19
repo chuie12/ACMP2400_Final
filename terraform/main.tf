1d732a2d89ff1e5a97858a2bf29569f9fe946562
chuie12 <chuie@unomaha.edu>
2026-04-19 14:17:18 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,10 +19,55 @@ provider "azurerm" {
   features {}
 }
 
+variable "image_tag" {
+type = string
+default = "latest"
+}
+
+variable "django_secret_key" {
+type = string
+sesitive = true
+}
+
 resource "azurerm_container_registry" "teacher_acr" {
   name = "acrteacheracmp2400"
   resource_group_name = "rg-chuie"
   location = "Central US"
   sku = "Basic"
   admin_enabled = false
+}
+
+resource "azurerm_container_group" "app" {
+  name = "cg-my-app"
+  location = "Central US"
+  resource_group_name = "rg-chuie"
+  os_type = "Linux"
+  ip_address_type = "Public"
+  dns_name_label = "chuie-myapp-demo"
+
+  container {
+    name = "my-app"
+    image = "acrteacheracmp2400.azurecr.io/my-app:${var.image_tag}"
+    cpu = 0.5
+    memory = 1.5
+
+    ports {
+      port     = 8000
+      protocol = "TCP"
+    }
+
+    environment_variables = {
+      DJANGO_SECRET_KEY = var.django_secret_key
+    }
+  }
+
+  image_registry_credential {
+    server = "acrteacheracmp2400.azurecr.io"
+    username = azurerm_container_registry.teacher_acr.admin_username
+    password = azurerm_container_registry.teacher_acr.admin_password
+  }
+}
+
+output "container_fqdn" {
+  value = azurerm_container_group.app.fqdn
 }
